00a676f6527b87ee52455e70b516534f5abb671a
timiolayinka <timiolayinka@hollandandbarrett.com>
2026-01-21 21:16:26 +0000

    Cleaning repo

--- Pipelines/factinventoryreceipt_IncrExtract.sql
+++
@@ -1,6 +0,0 @@
-BEGIN 
-	SELECT COALESCE(FORMAT_DATETIME('%F %H:%M:%E3S', MAX(Receipt_DateTime)),'1900-01-01 00:00:00.000') AS Maximum 
-	FROM `{{lake_project_id}}.OperationsCS_DWH.factinventoryreceipt`;
-EXCEPTION 
-	WHEN ERROR THEN SELECT '1900-01-08 00:00:00.000';
-END;
